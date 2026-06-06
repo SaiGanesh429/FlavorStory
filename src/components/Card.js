d21252d4e7bb5db4704100966ddf42561d924b61
Sai Ganesh <kanigolla.saiganesh@gmail.com>
2026-06-06 18:16:16 +0530

    added the customized css

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,29 +2,32 @@ import { CDN_URL } from "../utils/constants";
 
 const Card = (props) => {
   const { resData } = props;
+  const { name, cuisines, costForTwo, avgRating, cloudinaryImageId, sla } =
+    resData.card.card.info;
+    
   return (
     <div className="card">
       <div>
         <div className="card-image-wrap">
           <img
             className="card-image"
-            src={CDN_URL + resData.card.card.info.cloudinaryImageId}
+            src={CDN_URL + cloudinaryImageId}
             alt="Card Image"
           />
           <span className="card-badge">Bestseller</span>
         </div>
         <div className="card-content">
           <div key={resData.card.card.info.id}>
-            <p className="res-name">{resData.card.card.info.name}</p>
+            <p className="res-name">{name}</p>
             <p className="res-subtitle">
-              {resData.card.card.info.cuisines.join(" • ")} •{" "}
-              {resData.card.card.info.costForTwo}
+              {cuisines.join(" • ")} •{" "}
+              {costForTwo}
             </p>
             <div className="card-info">
               <span className="rating">
-                ⭐ {resData.card.card.info.avgRating}
+                ⭐ {avgRating}
               </span>
-              <span className="time">{props.deliveryTime}</span>
+              <span className="time">{sla.deliveryTime} mins</span>
               <span className="delivery">Free delivery</span>
             </div>
           </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import Header from "./Header";
 import CardList from "./CardList";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <Header></Header>hh
-
-      <CardList></CardList>
+      <Header />
+      <div className="button-wrapper">
+        <button className="top-rated-btn">Top Rated Restaurants</button>
+      </div>
+      <CardList />
     </div>
   );
 };
